3da0a5103f3bc7a7d16e3c5067b13556a170d057
user <talgatkyzys9@gmail.com>
2024-11-18 11:52:42 +0500

    init

---
+++ src/test/java/kz/iitu/zoopark/ZooparkApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.iitu.zoopark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooparkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
