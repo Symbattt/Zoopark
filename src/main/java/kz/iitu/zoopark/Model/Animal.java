aed39dd5386286fc25aba5aa0b39e7c8908f68f5
user <talgatkyzys9@gmail.com>
2024-11-20 22:55:45 +0500

    init

--- src/main/java/kz/iitu/zoopark/Model/Animal.java
+++ src/main/java/kz/iitu/zoopark/Model/Animal.java
@@ -1,4 +1,68 @@
 package kz.iitu.zoopark.Model;
 
+import javax.persistence.*;
+
+@Entity
 public class Animal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String species;
+    private String type; // Например, котенок, рыба и т.д.
+    private double weight; // Вес в кг
+    private double price; // Цена за вход
+
+    // Конструкторы, геттеры и сеттеры
+
+    public Animal() {
+    }
+
+    public Animal(String species, String type, double weight, double price) {
+        this.species = species;
+        this.type = type;
+        this.weight = weight;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
--- src/main/java/kz/iitu/zoopark/Repository/AnimalRepository.java
+++ src/main/java/kz/iitu/zoopark/Repository/AnimalRepository.java
@@ -1,4 +1,6 @@
 package kz.iitu.zoopark.Repository;
+import kz.iitu.zoopark.Model.Animal;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public class AnimalRepository {
+public interface AnimalRepository extends JpaRepository<Animal, Long> {
 }
