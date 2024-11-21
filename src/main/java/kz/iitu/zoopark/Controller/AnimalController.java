2212dca81502bcbe65dcfdf8efc9a607e7a7a6c1
user <talgatkyzys9@gmail.com>
2024-11-21 20:27:31 +0500

    init

--- src/main/java/kz/iitu/zoopark/Controller/AnimalController.java
+++ src/main/java/kz/iitu/zoopark/Controller/AnimalController.java
@@ -1,4 +1,41 @@
 package kz.iitu.zoopark.Controller;
+import kz.iitu.zoopark.Model.Animal;
+import kz.iitu.zoopark.Service.AnimalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/animals")
 public class AnimalController {
+
+    @Autowired
+    private AnimalService animalService;
+
+    @GetMapping
+    public List<Animal> getAllAnimals() {
+        return animalService.getAllAnimals();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Animal> getAnimalById(@PathVariable Long id) {
+        return animalService.getAnimalById(id);
+    }
+
+    @PostMapping
+    public Animal createAnimal(@RequestBody Animal animal) {
+        return animalService.createAnimal(animal);
+    }
+
+    @PutMapping("/{id}")
+    public Animal updateAnimal(@PathVariable Long id, @RequestBody Animal animal) {
+        return animalService.updateAnimal(id, animal);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAnimal(@PathVariable Long id) {
+        animalService.deleteAnimal(id);
+    }
 }
--- src/main/java/kz/iitu/zoopark/Service/AnimalService.java
+++ src/main/java/kz/iitu/zoopark/Service/AnimalService.java
@@ -1,4 +1,36 @@
 package kz.iitu.zoopark.Service;
+import kz.iitu.zoopark.Model.Animal;
+import kz.iitu.zoopark.Repository.AnimalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class AnimalService {
+
+    @Autowired
+    private AnimalRepository animalRepository;
+
+    public List<Animal> getAllAnimals() {
+        return animalRepository.findAll();
+    }
+
+    public Optional<Animal> getAnimalById(Long id) {
+        return animalRepository.findById(id);
+    }
+
+    public Animal createAnimal(Animal animal) {
+        return animalRepository.save(animal);
+    }
+
+    public Animal updateAnimal(Long id, Animal animal) {
+        animal.setId(id);
+        return animalRepository.save(animal);
+    }
+
+    public void deleteAnimal(Long id) {
+        animalRepository.deleteById(id);
+    }
 }
